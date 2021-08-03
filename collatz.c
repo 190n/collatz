0a6a80de1534ae88a16311c5a8273302dc4daebc
Ben Grant <benjamin.j.grant@gmail.com>
2021-08-03 12:05:15 -0700

    Initial single-threaded implementation

--- collatz.c
+++ collatz.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <inttypes.h>
+
+uint64_t num_iters(uint64_t x) {
+    uint64_t iters = 0;
+
+    do {
+        if (x % 2) {
+            // odd
+            x = x * 3 + 1;
+        } else {
+            // even
+            x /= 2;
+        }
+
+        iters++;
+    } while (x != 1);
+
+    return iters;
+}
 
 int main(void) {
-    printf("Hello, world!\n");
+    printf("Let's gooooo\n");
+
+    uint64_t max_iters = 0;
+
+    for (uint64_t i = 1; i < 1000000; i++) {
+        uint64_t iters = num_iters(i);
+        if (iters > max_iters) {
+            printf("%" PRIu64 ":%" PRIu64 "\n", i, iters);
+            max_iters = iters;
+        }
+    }
+
+    return 0;
 }
